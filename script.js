d391c2a7068544bf9b389fda62020f6d401437f5
karan80ff-bit <karan80ff@gmail.com>
2026-01-13 12:17:49 +0530

    Task 3

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Portfolio Loaded Successfully");
+function addTask() {
+const taskInput = document.getElementById("taskInput");
+const taskText = taskInput.value.trim();
+
+
+if (taskText === "") return;
+
+
+const li = document.createElement("li");
+li.textContent = taskText;
+
+
+li.onclick = function () {
+li.classList.toggle("completed");
+};
+
+
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "X";
+deleteBtn.onclick = function () {
+li.remove();
+};
+
+
+li.appendChild(deleteBtn);
+document.getElementById("taskList").appendChild(li);
+
+
+taskInput.value = "";
+}
